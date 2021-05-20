08c9c90557126d8876cfcab083ac6a27153b6c33
Maple Xu <maplexu2010@gmail.com>
2021-05-20 16:59:44 -0700

    finished setup part of script

--- setup.py
+++ setup.py
@@ -9,14 +9,6 @@ setup(
     version="1.0",
     author="Maple Xu",
     author_email="maplexu2010@gmail.com",
-    url="https://gitlab.eecs.umich.edu/akamil/canvas-tools",
-    license="FIXME",
-    packages=["create-web-app"],
-
-    # Python command line utilities will be installed in a PATH-accessible bin/
-    entry_points={
-        'console_scripts': [
-            'create-web-app = src.create-web-app:main',
-        ]
-    },
+    url="https://github.com/xumaple/create-web-app",
+    scripts=['create-web-app']
 )
